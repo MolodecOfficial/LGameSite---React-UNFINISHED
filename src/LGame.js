0149f30794d368e26dd0f168e476f389d00553bc
Molodec_Official <maksimkq1990@mail.ru>
2024-01-26 20:36:33 +0500

    next lvl -> style in js file

--- src/LGame.js
+++ src/LGame.js
@@ -1,5 +1,6 @@
 import './LGame.css';
 import './LGame_script'
+import mastertech from './assets/mastertech.jpg'
 
 
 
@@ -13,11 +14,29 @@ function LGame() {
                       <button className="main-button">На главную</button>
                       <button className="main-button">Сборки</button>
                       <button className="main-button">Команда</button>
-                      <button className="main-button">О нас</button>
+                      <button className="main-button">О студии</button>
                       <div className="log-reg">
                           <button type="button" className="log-reg-btn">Регистрация</button>
                           <button type="button" className="log-reg-dnld">Скачать лаунчер</button>
                       </div>
+                  </div>
+                  <div className="main-text">
+                      <p className="text">Автор этого сайта накурился и забыл, как ставить текст в определённую точку</p>
+                      <div className="mt">
+                          <img
+                              src={mastertech}
+                              style={{
+                                  zIndex: 1,
+                                  position: "absolute",
+                                  top: 225,
+                                  left: 'calc(120vh)',
+                                  justifyContent: "center",
+                                  width: 600,
+                                  height: 400,
+                                  borderRadius: 20}}
+                          />
+                      </div>
+                      <p className="text2">ооо нихуя се МТ, пойду поиграю --></p>
                   </div>
               </div>
           </div>
